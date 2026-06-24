59fea78e9a5095d9084fb78e39e95295e2cc25f3
karanioty <karan11052007@gmail.com>
2026-06-24 21:22:16 +0530

    Add solution for counting special characters
    Implement solution to count special characters in a string.

---
+++ String/3120. Count the Number of Special Characters I.py
@@ -0,0 +1,26 @@
+'''3120. Count the Number of Special Characters I
+""Example:
+Input: word = "aaAbcBC"
+Output: 3
+Explanation:
+The special characters in word are 'a', 'b', and 'c'.'''
+
+#code link: https://leetcode.com/problems/count-the-number-of-special-characters-i/description/?envType=problem-list-v2&envId=string
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        l=""
+        l1=""
+        count=0
+        for i in word:
+            if i in word.upper() and i not in l:
+                l+=i
+            elif i not in  l1:
+                l1+=i
+        
+        for i in l:
+            if i.lower() in l1:
+                count+=1
+        return count
+
+        
+        
