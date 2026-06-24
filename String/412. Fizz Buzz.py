98b2b0b6174098c632d2613d0382d367a6b3ece0
karanioty <karan11052007@gmail.com>
2026-06-24 21:10:25 +0530

    Add Fizz Buzz problem solution
    Implement Fizz Buzz solution in Python.

---
+++ String/412. Fizz Buzz.py
@@ -0,0 +1,18 @@
+'''412. Fizz Buzz
+""Example:
+Input: n = 3
+Output: ["1","2","Fizz"]'''
+#code link: https://leetcode.com/problems/fizz-buzz/description/?envType=problem-list-v2&envId=string
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        l=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                l.append("FizzBuzz")
+            elif i%3==0:
+                l.append("Fizz")
+            elif i%5==0:
+                l.append("Buzz")
+            else:
+                l.append(str(i))
+        return l
