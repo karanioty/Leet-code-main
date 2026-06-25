e10694d9a80cec77b88b7de85efc30690c52f533
karanioty <karan11052007@gmail.com>
2026-06-25 11:58:40 +0530

    Create 169. Majority Element.py

---
+++ hashtable/169. Majority Element.py
@@ -0,0 +1,17 @@
+'''169. Majority Element
+""Example:
+Input: nums = [3,2,3]
+Output: 3'''
+#code link: https://leetcode.com/problems/majority-element/description/?envType=problem-list-v2&envId=hash-table
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        m={}
+        for i in range(len(nums)):
+            m[nums[i]]=m.get(nums[i],0)+1
+        k=max(m.values())
+        d=0
+        for i,j in m.items():
+            if j==k:
+                d=i
+                break
+        return d
