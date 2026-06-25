15522cf1f61b10bf61df7956ac0c28b0a622978e
karanioty <karan11052007@gmail.com>
2026-06-25 14:43:20 +0530

    Add solution for Largest Substring Between Two Equal Characters
    Implement solution for finding largest substring between equal characters.

---
+++ hashtable/1624. Largest Substring Between Two Equal Characters.py
@@ -0,0 +1,16 @@
+'''1624. Largest Substring Between Two Equal Characters
+""Example:
+Input: s = "aa"
+Output: 0
+Explanation: The optimal substring here is an empty substring between the two 'a's.'''
+#code link: https://leetcode.com/problems/largest-substring-between-two-equal-characters/description/?envType=problem-list-v2&envId=hash-table
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        f_i={}
+        ans=-1
+        for i,ch in enumerate(s):
+            if ch in f_i:
+                ans=max(ans,i-f_i[ch]-1)
+            else:
+                f_i[ch]=i
+        return ans
