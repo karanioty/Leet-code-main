ed00ae7798f2559fe30836e3ef26201fba2571ee
karanioty <karan11052007@gmail.com>
2026-08-21 09:11:44 +0530

    Update examples in 2D Matrix II problem statement
    Added example input and output for target 20.

--- array/240. Search a 2D Matrix II.py
+++ array/240. Search a 2D Matrix II.py
@@ -1,7 +1,10 @@
 '''240. Search a 2D Matrix II
 ""Example:
 Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
-Output: true'''
+Output: true
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+'''
 #code link: https://leetcode.com/problems/search-a-2d-matrix-ii/description/?envType=problem-list-v2&envId=array
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
